fe83b336b89ba887d369a75e49ccc5f6d44eb326
Ace2009 <95550090+Ace2009@users.noreply.github.com>
2021-12-04 23:29:54 +0100

    Add files via upload

---
+++ opasna rabota.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+    int a;
+
+    int b;
+
+    int l;
+
+    int p;
+
+    cout<<"КАЛКУЛАТОР"<<endl;
+
+    cout<<"          "<<endl;
+
+    cout<<"Внесете вредност за страната А";
+    cin>>a;
+
+    cout<<"Внесете вредност за страната Б";
+    cin>>b;
+
+    l=a+b;
+
+    p=a*b;
+    cout<<"         "<<endl;
+
+    cout<<"Периметарот на фигурата е  "<<l<<endl;
+
+    cout<<"Плоштината на фигурата е ";
+    cout<<p<<endl;
+    cout<<"         "<<endl;
+    
+    cout<<"instagram:mitkovski_11"<<endl;
+    cout<<"TikTok:Elzi83"<<endl;
+    cout<<"Email:aleksandar.mitkovski4@gmail.com"<<endl;
+
+
+
+    system ("pause");
+    return 0;
+
+}
