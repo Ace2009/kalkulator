34f8b78a7cbe7d0d5c0c7029235ff8a06ce2abfc
Ace2009 <95550090+Ace2009@users.noreply.github.com>
2021-12-05 08:39:12 +0100

    Add files via upload

---
+++ POLUPRECNIK.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+int main() {
+
+double pp,o,p;
+
+cout<<"vnesete vrednost na poluprecnikot(pp)";
+
+cin>>pp;
+
+o=2*pp*M_PI;
+p=pp*pp*M_PI;
+
+cout<<"p="<<p<<endl;
+cout<<"o="<<o<<endl;
+
+
+system ("pause");
+    return 0;
+    
+}
